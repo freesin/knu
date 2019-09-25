a85eab010657392ee1ca858124ce238ea2810832
freesin <freesin94@gmail.com>
2019-09-25 19:31:36 +0900

    차트 생성

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from "./Header.js"
+import DataTable from "./DataTable.js"
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
